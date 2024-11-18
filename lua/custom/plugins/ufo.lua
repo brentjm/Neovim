8ac059a733eae88d0b778cb9248796e56c82ace9
Brent Maranzano <Brent.Maranzano@pfizer.com>
2024-11-18 18:31:44 -0500

    Added ufo plugin for better cold folding

---
+++ lua/custom/plugins/ufo.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+      -- Key mappings
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+      -- LSP capabilities
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
+      -- Setup LSP servers with folding capabilities
+      local language_servers = require('lspconfig').util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require('lspconfig')[ls].setup {
+          capabilities = capabilities,
+          -- you can add other fields for setting up lsp server in this table
+        }
+      end
+
+      -- Setup ufo
+      require('ufo').setup()
+    end,
+  },
+}
