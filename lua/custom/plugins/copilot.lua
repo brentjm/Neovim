0816c28630d18f9ba18a5a28bcbf05c4374a1a5b
Brent Maranzano <brent_maranzano@yahoo.com>
2025-06-30 18:08:01 -0400

    Update copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,12 +1,11 @@
 return {
-  'github/copilot.vim',
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'main',
     dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
+    build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
       prompts = {
